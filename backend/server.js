7a8f49cde3735717394ed2bb440f0c3bf3bc10cc
ganesh9803 <santhosh.ganesh1023@gmail.com>
2025-07-07 14:24:53 +0530

    intial modify

--- backend/server.js
+++ backend/server.js
@@ -23,4 +23,12 @@ app.use('/api/cart', cartRoutes);
 app.use('/api/orders', orderRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Error handling middleware
+app.get('/',(req,res)=> {
+  res.send("API Working")
+})
+
+app.listen(PORT , ()=>console.log('Sever started on PORT : ' + PORT ))
+
+export default app;
